88a2c85114c03aca0d53ee176e3a44dd740b58ba
tubleranel <jrrtuble@gmail.com>
2025-05-02 19:44:08 +0800

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,174 @@
+import fs from "fs/promises";
+import path from "path";
+import process from "process";
+import { authenticate } from "@google-cloud/local-auth";
+import { google, gmail_v1, Auth } from "googleapis";
+import { OAuth2Client } from "google-auth-library";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+import { fileURLToPath } from "url";
+import { json } from "stream/consumers";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"];
+const TOKEN_PATH = path.join(__dirname, "token.json");
+const CREDENTIALS_PATH = path.join(__dirname, "credentials.json");
+
+/**
+ * Reads previously authorized credentials from the saved file.
+ */
+const server = new McpServer({
+  name: "gmail",
+  version: "1.0.0",
+  capabilities: {
+    resources: {},
+    tools: {},
+  },
+});
+
+async function loadSavedCredentialsIfExist(): Promise<OAuth2Client | null> {
+  try {
+    const content = await fs.readFile(TOKEN_PATH, "utf-8");
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials) as OAuth2Client;
+  } catch (err) {
+    return null;
+  }
+}
+
+/**
+ * Serializes credentials to a file compatible with GoogleAuth.fromJSON.
+ */
+async function saveCredentials(client: OAuth2Client): Promise<void> {
+  const content = await fs.readFile(CREDENTIALS_PATH, "utf-8");
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: "authorized_user",
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request authorization to call APIs.
+ */
+async function authorize(): Promise<OAuth2Client> {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+
+  const newClient = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+
+  if (newClient.credentials) {
+    await saveCredentials(newClient);
+  }
+
+  return newClient;
+}
+
+/**
+ * Lists the labels in the user's account.
+ */
+
+async function listLabels<T>(
+  auth: OAuth2Client
+): Promise<gmail_v1.Schema$Label[] | null> {
+  const gmail = google.gmail({ version: "v1", auth });
+  const res = await gmail.users.labels.list({ userId: "me" });
+  const labels: gmail_v1.Schema$Label[] | undefined = res.data.labels;
+
+  if (!labels || labels.length === 0) {
+    console.log("No labels found.");
+    return null;
+  }
+
+  return labels;
+}
+
+// Run the script
+// authorize()
+//   .then(async (auth) => {
+//     const labels = await listLabels(auth);
+//     labels?.forEach((label) => {
+//       console.log(`Label ID: ${label.id}, Name: ${label.name}`);
+//     });
+//   })
+//   .catch(console.error);
+
+function formatLabels(labels: gmail_v1.Schema$Label): string {
+  return [
+    `ID: ${labels.id || "Unknown"}`,
+    `Name: ${labels.name || "Unknown"}`,
+    `Message List Visibility: ${labels.messageListVisibility || "Unknown"}`,
+    `Label List Visibility: ${labels.labelListVisibility || "Unknown"}`,
+    `Type: ${labels.type || "No headline"}`,
+    "---",
+  ].join("\n");
+}
+
+server.tool("get-labels", "Get Labels from GMail", async ({}) => {
+  const auth = await authorize();
+  const labels = await listLabels(auth);
+
+  const jsonLabels = JSON.stringify(labels);
+
+  if (!jsonLabels) {
+    return {
+      content: [
+        {
+          type: "text",
+          text: "Failed to retrieve labels",
+        },
+      ],
+    };
+  }
+
+  const formattedLabels = JSON.parse(jsonLabels).map(formatLabels);
+  const labelsText = `Active Labels for :\n\n${formattedLabels.join("\n")}`;
+
+  return {
+    content: [
+      {
+        type: "text",
+        text: labelsText,
+      },
+    ],
+  };
+});
+
+async function main() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+  console.error("Credentials Path: ", CREDENTIALS_PATH);
+  console.error("GMail MCP Server running on stdio");
+}
+
+main().catch((error) => {
+  console.error("Fatal error in main():", error);
+  process.exit(1);
+});
+
+// async function main() {
+//   const auth = await authorize();
+//   const labels = await listLabels(auth);
+
+//   const jsonLabels = JSON.stringify(labels);
+
+//   console.log(jsonLabels);
+// }
+
+// main().catch((error) => {
+//   console.error("Fatal error in main():", error);
+//   process.exit(1);
+// });
